103d482fa0a0afbc6a16a7ef376e47047a5bb008
fahadhafizdev <fahadhafiz.dev@gmail.com>
2022-08-06 19:27:55 +0700

    Update main_page.dart

--- lib/ui/page/home/main_page.dart
+++ lib/ui/page/home/main_page.dart
@@ -16,100 +16,178 @@ class MainPage extends StatelessWidget {
     }
 
     Widget cardButton() {
-      return FloatingActionButton(
-        onPressed: () {
-          showDialogComing();
-        },
-        backgroundColor: cYellow,
-        child: Image.asset(
-          'assets/icons/icon_search.png',
-          height: 25,
-        ),
-      );
-    }
-
-    Widget customBottomNav() {
-      return ClipRRect(
-        borderRadius: const BorderRadius.vertical(
-          top: Radius.circular(30),
-        ),
-        child: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          clipBehavior: Clip.antiAlias,
-          notchMargin: 10,
-          child: BottomNavigationBar(
-            currentIndex: 0, //index
-            onTap: (value) {
-              print(value);
-              // context.read<PageCubit>().setPage(value);
-            },
-            backgroundColor: cBlackBold,
-            type: BottomNavigationBarType.fixed,
-            items: [
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: const EdgeInsets.only(
-                    top: 20,
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth <= 600) {
+            return FloatingActionButton(
+              onPressed: () {
+                showDialogComing();
+              },
+              backgroundColor: cYellow,
+              child: Image.asset(
+                'assets/icons/icon_search.png',
+                height: 25,
+              ),
+            );
+          } else {
+            return Column(
+              children: [
+                const SizedBox(height: 40),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialogComing();
+                  },
+                  backgroundColor: cYellow,
+                  child: Image.asset(
+                    'assets/icons/icon_search.png',
+                    height: 25,
                   ),
+                ),
+                SizedBox(height: 20),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialogComing();
+                  },
+                  backgroundColor: cBlackBold,
                   child: Image.asset(
                     'assets/icons/icon_home.png',
                     height: 25,
                   ),
                 ),
-                label: '',
-                tooltip: 'home',
-              ),
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: const EdgeInsets.only(
-                    top: 20,
-                  ),
+                SizedBox(height: 20),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialogComing();
+                  },
+                  backgroundColor: cBlackBold,
                   child: Image.asset(
                     'assets/icons/icon_disc.png',
                     height: 25,
                   ),
                 ),
-                label: '',
-                tooltip: 'voucher',
-              ),
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: const EdgeInsets.only(
-                    top: 20,
-                  ),
+                SizedBox(height: 20),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialogComing();
+                  },
+                  backgroundColor: cBlackBold,
                   child: Image.asset(
                     'assets/icons/icon_keranjang.png',
                     height: 25,
                   ),
                 ),
-                label: '',
-                tooltip: 'shop',
-              ),
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: const EdgeInsets.only(
-                    top: 20,
-                  ),
+                SizedBox(height: 20),
+                FloatingActionButton(
+                  onPressed: () {
+                    showDialogComing();
+                  },
+                  backgroundColor: cBlackBold,
                   child: Image.asset(
                     'assets/icons/icon_user.png',
                     height: 25,
                   ),
                 ),
-                label: '',
-                tooltip: 'account',
-              ),
-            ],
-          ),
-        ),
+              ],
+            );
+          }
+        },
       );
     }
 
-    return Scaffold(
-      backgroundColor: cBlack,
-      bottomNavigationBar: customBottomNav(),
-      floatingActionButton: cardButton(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: HomePage(),
+    Widget customBottomNav() {
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          return (constraints.maxWidth <= 600)
+              ? ClipRRect(
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(30),
+                  ),
+                  child: BottomAppBar(
+                    shape: const CircularNotchedRectangle(),
+                    clipBehavior: Clip.antiAlias,
+                    notchMargin: 10,
+                    child: BottomNavigationBar(
+                      currentIndex: 0, //index
+                      onTap: (value) {
+                        print(value);
+                        // context.read<PageCubit>().setPage(value);
+                      },
+                      backgroundColor: cBlackBold,
+                      type: BottomNavigationBarType.fixed,
+                      items: [
+                        BottomNavigationBarItem(
+                          icon: Container(
+                            margin: const EdgeInsets.only(
+                              top: 20,
+                            ),
+                            child: Image.asset(
+                              'assets/icons/icon_home.png',
+                              height: 25,
+                            ),
+                          ),
+                          label: '',
+                          tooltip: 'home',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Container(
+                            margin: const EdgeInsets.only(
+                              top: 20,
+                            ),
+                            child: Image.asset(
+                              'assets/icons/icon_disc.png',
+                              height: 25,
+                            ),
+                          ),
+                          label: '',
+                          tooltip: 'voucher',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Container(
+                            margin: const EdgeInsets.only(
+                              top: 20,
+                            ),
+                            child: Image.asset(
+                              'assets/icons/icon_keranjang.png',
+                              height: 25,
+                            ),
+                          ),
+                          label: '',
+                          tooltip: 'shop',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Container(
+                            margin: const EdgeInsets.only(
+                              top: 20,
+                            ),
+                            child: Image.asset(
+                              'assets/icons/icon_user.png',
+                              height: 25,
+                            ),
+                          ),
+                          label: '',
+                          tooltip: 'account',
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              : SizedBox();
+        },
+      );
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          backgroundColor: cBlack,
+          bottomNavigationBar: customBottomNav(),
+          floatingActionButton: cardButton(),
+          floatingActionButtonLocation: (constraints.maxWidth <= 600)
+              ? FloatingActionButtonLocation.centerDocked
+              : FloatingActionButtonLocation.miniEndTop,
+          body: HomePage(),
+        );
+      },
     );
   }
 }
