96986258bc66720d8dda14978990f13447e26bf3
fahadhafizdev <fahadhafiz.dev@gmail.com>
2022-08-17 15:24:59 +0700

    Update profile_page.dart

--- lib/ui/page/home/profile_page.dart
+++ lib/ui/page/home/profile_page.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/shared/theme.dart';
+import 'package:lottie/lottie.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LottieBuilder.asset(
+            'assets/lottie/empty-state-3.json',
+            repeat: true,
+            width: 300,
+          ),
+          const SizedBox(height: 12),
+          ConstrainedBox(
+            constraints: const BoxConstraints(
+              maxWidth: 400,
+            ),
+            child: Text(
+              'Profile page is coming soon',
+              textAlign: TextAlign.center,
+              style: tWhite.copyWith(
+                fontWeight: medium,
+                fontSize: 18,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
