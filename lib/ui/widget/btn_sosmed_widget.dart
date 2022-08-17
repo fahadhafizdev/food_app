22ca963e0a5a95031708c8d0ec7d957e3e5568da
fahadhafizdev <fahadhafiz.dev@gmail.com>
2022-08-17 21:46:47 +0700

    update

--- lib/ui/widget/btn_sosmed_widget.dart
+++ lib/ui/widget/btn_sosmed_widget.dart
@@ -9,62 +9,74 @@ class ButtonSosmed extends StatelessWidget {
     return Row(
       children: [
         Expanded(
-          child: Container(
-            height: 42,
-            margin: EdgeInsets.only(left: lebar * 0.05, right: 6),
-            decoration: BoxDecoration(
-              color: cGrey2,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    width: 18,
-                  ),
-                  Image.asset(
-                    'assets/icons/icon_google.png',
-                    height: 20,
-                  ),
-                  const SizedBox(width: 11),
-                  Text(
-                    'Google',
-                    style: tGrey.copyWith(fontWeight: bold, fontSize: 13),
-                  ),
-                ],
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/main-page', (route) => false);
+            },
+            child: Container(
+              height: 42,
+              margin: EdgeInsets.only(left: lebar * 0.05, right: 6),
+              decoration: BoxDecoration(
+                color: cGrey2,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      width: 18,
+                    ),
+                    Image.asset(
+                      'assets/icons/icon_google.png',
+                      height: 20,
+                    ),
+                    const SizedBox(width: 11),
+                    Text(
+                      'Google',
+                      style: tGrey.copyWith(fontWeight: bold, fontSize: 13),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
         Expanded(
-          child: Container(
-            height: 42,
-            margin: EdgeInsets.only(left: 6, right: lebar * 0.05),
-            decoration: BoxDecoration(
-              color: cGrey2,
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    width: 18,
-                  ),
-                  Image.asset(
-                    'assets/icons/icon_fb.png',
-                    height: 20,
-                  ),
-                  const SizedBox(width: 11),
-                  Text(
-                    'Facebook',
-                    style: tGrey.copyWith(
-                      fontWeight: bold,
-                      fontSize: 13,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, '/main-page', (route) => false);
+            },
+            child: Container(
+              height: 42,
+              margin: EdgeInsets.only(left: 6, right: lebar * 0.05),
+              decoration: BoxDecoration(
+                color: cGrey2,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      width: 18,
                     ),
-                  ),
-                ],
+                    Image.asset(
+                      'assets/icons/icon_fb.png',
+                      height: 20,
+                    ),
+                    const SizedBox(width: 11),
+                    Text(
+                      'Facebook',
+                      style: tGrey.copyWith(
+                        fontWeight: bold,
+                        fontSize: 13,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
